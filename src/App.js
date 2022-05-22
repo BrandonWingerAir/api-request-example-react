fe188b6d66dcde8b377f2b87bd1803463df4ad66
Brandon Winger-Air <brandonwinger-air@outlook.com>
2022-05-22 17:08:56 -0400

    add structure, style and axios functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React, { useState } from 'react';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
-  return <div>Hello world!</div>;
+  const [quote, setQuote] = useState("");
+
+  const getQuote = () => {
+    Axios.get("https://api.kanye.rest/").then((response) => {
+      setQuote("\"" + response.data.quote + "\" - ");
+    });
+  };
+
+  return (
+    <div className='container'>
+      <div>
+        {quote} Kanye West
+        <button onClick={getQuote}>Random Quote</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
